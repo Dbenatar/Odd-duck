c86223005b7c26db3f3fdd4b73d1be9033054a3b
Dominic <dom.benatar@gmail.com>
2023-10-02 16:52:31 +0100

    js added far from done

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-// write a constructor function
-
-// show 3 random images . src tags with alt tags
-
 let productContainer = document.querySelector("section");
-let image1 = documnet.querySelector("section img:first-child");
+let image1 = document.querySelector("section img:first-child");
 let image2 = document.querySelector("section img:nth-child(2)");
 let image3 = document.querySelector("section img:nth-child(3)");
 
-function Product(name, src) {
+function Products(name, src) {
   this.name = name;
   this.src = src;
   this.views = views;
@@ -23,7 +19,7 @@ function renderProducts() {
   let product2Index = getRandomIndex();
   let product3Index = getRandomIndex();
 
-  while (product1Index == product2Index || product3Index) {
+  while (product1Index === product2Index || product3Index) {
     product1Index = getRandomIndex();
   }
 
@@ -47,35 +43,34 @@ function handleProductClick(event) {
   } else {
     renderProducts();
   }
-}
 
-for (let i = 0; i < allProducts.length; i++) {
-  if (clickedProduct === allProducts[i].name) {
-    allProducts[i].clicks++;
-    break;
+  for (let i = 0; i < allProducts.length; i++) {
+    if (clickedProduct === allProducts[i].name) {
+      allProducts[i].clicks++;
+      break;
+    }
   }
 }
-
 const allProducts = [
-  new.Products("bag", "./assets/bag.jpg"),
-  new.Products("banana", "./assets/banana.jpg"),
-  new.Products("bathroom", "./assets/bathroom.jpg"),
-  new.Products("boots", "./assets/boots.jpg"),
-  new.Products("breakfast", "./assets/breakfast.jpg"),
-  new.Products("bubblegum", "./assets/bubblegum.jpg"),
-  new.Products("chair", "./assets/chair.jpg"),
-  new.Products("cthulu", "./assets/cthulu.jpg"),
-  new.Products("dog-duck", "./assets/dog-duck.jpg"),
-  new.Products("dragon", "./assets/dragon.jpg"),
-  new.Products("pen", "./assets/pen.jpg"),
-  new.Products("pet-sweep", "./assets/pet-sweep.jpg"),
-  new.Products("scissors", "./assets/scissors.jpg"),
-  new.Products("shark", "./assets/shark.jpg"),
-  new.Products("sweep", "./assets/sweep.png"),
-  new.Products("tauntaun", "./assets/tauntaun.jpg"),
-  new.Products("unicorn", "./assets/unicorn.jpg"),
-  new.Products("water-can", "./assets/water-can.jpg"),
-  new.Products("wine-glass", "./assets/wine-glass.jpg"),
+  new Products("bag", "./assets/bag.jpg"),
+  new Products("banana", "./assets/banana.jpg"),
+  new Products("bathroom", "./assets/bathroom.jpg"),
+  new Products("boots", "./assets/boots.jpg"),
+  new Products("breakfast", "./assets/breakfast.jpg"),
+  new Products("bubblegum", "./assets/bubblegum.jpg"),
+  new Products("chair", "./assets/chair.jpg"),
+  new Products("cthulu", "./assets/cthulu.jpg"),
+  new Products("dog-duck", "./assets/dog-duck.jpg"),
+  new Products("dragon", "./assets/dragon.jpg"),
+  new Products("pen", "./assets/pen.jpg"),
+  new Products("pet-sweep", "./assets/pet-sweep.jpg"),
+  new Products("scissors", "./assets/scissors.jpg"),
+  new Products("shark", "./assets/shark.jpg"),
+  new Products("sweep", "./assets/sweep.png"),
+  new Products("tauntaun", "./assets/tauntaun.jpg"),
+  new Products("unicorn", "./assets/unicorn.jpg"),
+  new Products("water-can", "./assets/water-can.jpg"),
+  new Products("wine-glass", "./assets/wine-glass.jpg"),
 ];
 
 productContainer.addEventListener("click", handleProductClick);
