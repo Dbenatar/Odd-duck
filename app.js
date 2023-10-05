804f97c29aa6eaa81496a3ef8049eac9b683bea3
Dominic <dom.benatar@gmail.com>
2023-10-05 11:13:11 +0100

    chart fixed

--- app.js
+++ app.js
@@ -3,41 +3,50 @@ const productNames = [];
 const productClicks = [];
 const productViews = [];
 let userClicks = 0;
-let maxClicks = 25;
+const maxClicks = 25;
+
+const allProducts = [];
 
 const productContainer = document.querySelector("section");
 const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 
-function Products(name) {
+function Products(name, views, clicks) {
   this.name = name;
   this.src = `./assets/${name}.jpg`;
-  this.views = 0;
-  this.clicks = 0;
+  this.views = views;
+  this.clicks = clicks;
+
+  allProducts.push(this);
 }
 
-const allProducts = [
-  new Products("bag"),
-  new Products("banana"),
-  new Products("bathroom"),
-  new Products("boots"),
-  new Products("breakfast"),
-  new Products("bubblegum"),
-  new Products("chair"),
-  new Products("cthulu"),
-  new Products("dog-duck"),
-  new Products("dragon"),
-  new Products("pen"),
-  new Products("pet-sweep"),
-  new Products("scissors"),
-  new Products("shark"),
-  new Products("sweep"),
-  new Products("tauntaun"),
-  new Products("unicorn"),
-  new Products("water-can"),
-  new Products("wine-glass"),
-];
+if (localStorage.getItem("products") === null) {
+  new Products("bag", 0, 0);
+  new Products("banana", 0, 0);
+  new Products("bathroom", 0, 0);
+  new Products("boots", 0, 0);
+  new Products("breakfast", 0, 0);
+  new Products("bubblegum", 0, 0);
+  new Products("chair", 0, 0);
+  new Products("cthulu", 0, 0);
+  new Products("dog-duck", 0, 0);
+  new Products("dragon", 0, 0);
+  new Products("pen", 0, 0);
+  new Products("pet-sweep", 0, 0);
+  new Products("scissors", 0, 0);
+  new Products("shark", 0, 0);
+  new Products("sweep", 0, 0);
+  new Products("tauntaun", 0, 0);
+  new Products("unicorn", 0, 0);
+  new Products("water-can", 0, 0);
+  new Products("wine-glass", 0, 0);
+} else {
+  const productsLS = JSON.parse(localStorage.getItem("products"));
+  for (let i = 0; i < productsLS.length; i++) {
+    new Products(productsLS[i].name, productsLS[i].views, productsLS[i].clicks);
+  }
+}
 
 function getRandomIndex() {
   return Math.floor(Math.random() * allProducts.length);
@@ -56,7 +65,6 @@ function renderProducts() {
     product2Index = getRandomIndex();
     product3Index = getRandomIndex();
   }
-
   image1.src = allProducts[product1Index].src;
   image2.src = allProducts[product2Index].src;
   image3.src = allProducts[product3Index].src;
@@ -68,10 +76,11 @@ function renderProducts() {
   allProducts[product2Index].views++;
   allProducts[product3Index].views++;
 }
-
 function handleProductClick(event) {
   if (userClicks === maxClicks) {
     alert("You have run out of votes");
+    renderChart();
+    localStorage.setItem("products", JSON.stringify(allProducts));
     return;
   }
 
@@ -79,11 +88,11 @@ function handleProductClick(event) {
 
   let clickedProduct = event.target.alt;
 
-  // if (event.target === productContainer) {
-  //   alert("Please choose an image");
-  // } else {
-  //   renderProducts();
-  // }
+  if (event.target === productContainer) {
+    alert("Please choose an image");
+  } else {
+    renderProducts();
+  }
 
   for (let i = 0; i < allProducts.length; i++) {
     if (clickedProduct === allProducts[i].name) {
@@ -98,83 +107,54 @@ image1.addEventListener("Click", handleProductClick);
 image2.addEventListener("Click", handleProductClick);
 image3.addEventListener("Click", handleProductClick);
 
-function showResults() {
-  const results = document.getElementById("results");
+function renderChart() {
+  const ctx = document.getElementById("myChart");
+
+  const labels = [];
+  const views = [];
+  const clicks = [];
 
   for (let i = 0; i < allProducts.length; i++) {
-    const li = document.createElement("li");
-    const products = allProducts[i];
-    li.textContent = `${products.name} was viewed ${products.views} times, and clicked ${products.clicks} times`;
-    results.appendChild(li);
-    productNames.push(products.name);
-    productViews.push(products.views);
-    productClicks.push(products.clicks);
+    labels.push(allProducts[i].name);
+    views.push(allProducts[i].views);
+    clicks.push(allProducts[i].clicks);
   }
+  const config = {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "# of votes",
+          // for loop
+          data: clicks,
+          borderWidth: 3,
+          backgroundColor: ["skyblue"],
+        },
+        {
+          type: "bar",
+          label: "# of views",
+          data: views,
+          borderWidth: 3,
+          backgroundColor: ["red"],
+        },
+      ],
+    },
+  };
   new Chart(ctx, config);
 }
+
 productContainer.addEventListener("click", handleProductClick);
 
-const viewResults = document.getElementById("view-results");
-viewResults.addEventListener("click", showResults);
-const config = {
-  type: "bar",
-  data: {
-    labels: productNames,
-    datasets: [
-      {
-        label: "# of votes",
-        // for loop
-        data: productClicks,
-        borderWidth: 3,
-        backgroundColor: [
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-          "skyblue",
-        ],
-      },
-      {
-        type: "bar",
-        label: "# of views",
-        data: productViews,
-        borderWidth: 3,
-        backgroundColor: [
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-          "red",
-        ],
-      },
-    ],
-  },
-};
 renderProducts();
+
+// const viewResults = document.getElementById("view-results");
+// viewResults.addEventListener("click", showResults);
+
+const prodStringified = JSON.stringify(allProducts);
+
+localStorage.setItem("products", prodStringified);
+
+const prodFromLocal = localStorage.getItem("products");
+
+const prodParsed = JSON.parse(prodFromLocal);
